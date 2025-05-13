f6798cf91a3eb76b46a9e1dd7987407f049220a7
Erin Vergeer <e.vergeer@student.tudelft.nl>
2025-05-13 18:48:06 +0200

    clean up, make print information optional

--- src/conpot_iec104.py
+++ src/conpot_iec104.py
@@ -1,88 +1,31 @@
 import functools
 import time
 import c104
+from conpot_iec104_helper import *
 
-    ##################################
-    # CONNECTION STATE HANDLER
-    ##################################
+def is_conpot(my_client):
+    cl_ct_count = len(my_client.connections)
 
-def cl_ct_on_state_change(connection: c104.Connection, state: c104.ConnectionState) -> None:
-    print("CL] Connection State Changed {0} | State {1}".format(connection.originator_address, state))
+    if not cl_ct_count == 1:
+        return False
 
+    ct = my_client.connections[0]
+    ct_st_count = len(ct.stations)
 
+    if not ct_st_count == 1:
+        return False
 
-##################################
-# NEW DATA HANDLER
-##################################
+    st = ct.stations[0]
+    st_pt_count = len(st.points)
 
-def cl_pt_on_receive_point(point: c104.Point, previous_info: c104.Information, message: c104.IncomingMessage) -> c104.ResponseState:
-    print("CL] {0} REPORT on IOA: {1}, message: {2}, previous: {3}, current: {4}".format(point.type, point.io_address, message, previous_info, point.info))
-    # print("{0}".format(message.is_negative))
-    # print("-->| POINT: 0x{0} | EXPLAIN: {1}".format(message.raw.hex(), c104.explain_bytes(apdu=message.raw)))
-    return c104.ResponseState.SUCCESS
+    if not st_pt_count == 59:
+        return False
 
+    if not st.common_address == 7720:
+        return False
+    
+    return True
 
-##################################
-# NEW OBJECT HANDLER
-##################################
-
-def cl_on_new_station(client: c104.Client, connection: c104.Connection, common_address: int, custom_arg: str, y: str = "default value") -> None:
-    print("CL] NEW STATION {0} | CLIENT OA {1}".format(common_address, client.originator_address))
-    connection.add_station(common_address=common_address)
-
-
-def cl_on_new_point(client: c104.Client, station: c104.Station, io_address: int, point_type: c104.Type) -> None:
-    print("CL] NEW POINT: {1} with IOA {0} | CLIENT OA {2}".format(io_address, point_type, client.originator_address))
-    point = station.add_point(io_address=io_address, type=point_type)
-    point.on_receive(callable=cl_pt_on_receive_point)
-
-
-
-
-
-##################################
-# RAW MESSAGE HANDLER
-##################################
-
-def cl_ct_on_receive_raw(connection: c104.Connection, data: bytes) -> None:
-    print("CL] <-in-- {1} [{0}] | CONN OA {2}".format(data.hex(), c104.explain_bytes_dict(apdu=data), connection.originator_address))
-
-
-def cl_ct_on_send_raw(connection: c104.Connection, data: bytes) -> None:
-    print("CL] -out-> {1} [{0}] | CONN OA {2}".format(data.hex(), c104.explain_bytes_dict(apdu=data), connection.originator_address))
-
-
-
-
-##################################
-# Dump points
-##################################
-
-def cl_dump():
-    global my_client, cl_connection_1, conpot
-    if cl_connection_1.is_connected:
-        print("")
-        cl_ct_count = len(my_client.connections)
-        print("CL] |--+ CLIENT has {0} connections".format(cl_ct_count))
-        for ct_iter in range(cl_ct_count):
-            ct = my_client.connections[ct_iter]
-            ct_st_count = len(ct.stations)
-            print("       |--+ CONNECTION has {0} stations".format(ct_st_count))
-            for st_iter in range(ct_st_count):
-                st = ct.stations[st_iter]
-                st_pt_count = len(st.points)
-
-                if int(st.common_address) == 7720 and st_pt_count == 59:
-                    conpot = True
-
-                print("          |--+ STATION {0} has {1} points".format(st.common_address, st_pt_count))
-                print("             |      TYPE      |   IOA   |       VALUE        |        PROCESSED AT        |        RECORDED  AT        |      QUALITY      ")
-                print("             |----------------|---------|--------------------|----------------------------|----------------------------|-------------------")
-                for pt_iter in range(st_pt_count):
-                    pt = st.points[pt_iter]
-                    print("             | %s | %7s | %18s | %26s | %26s | %s" % (pt.type, pt.io_address, pt.value, pt.processed_at.isoformat(),
-                                                                                 pt.recorded_at and pt.recorded_at.isoformat() or 'N. A.', pt.quality))
-                    print("             |----------------|---------|--------------------|----------------------------|----------------------------|-------------------")
 
 
 def test(address):
@@ -91,45 +34,31 @@ def test(address):
     :param address: The IP address of the host.
     :return: True if the signature is found, False otherwise.
     """
-    global my_client, cl_connection_1
-    c104.set_debug_mode(mode=c104.Debug.Client|c104.Debug.Connection)
-    print("CL] DEBUG MODE: {0}".format(c104.get_debug_mode()))
+    ### Debug mode
+    #c104.set_debug_mode(mode=c104.Debug.Client|c104.Debug.Connection)
+    #print("CL] DEBUG MODE: {0}".format(c104.get_debug_mode()))
 
     my_client = c104.Client(tick_rate_ms=1000, command_timeout_ms=5000)
     my_client.originator_address = 123
     cl_connection_1 = my_client.add_connection(ip=address, port=2404, init=c104.Init.ALL)
 
-    cl_connection_1.on_state_change(callable=cl_ct_on_state_change)
 
-    my_client.on_new_station(callable=functools.partial(cl_on_new_station,
-                                                        custom_arg="extra argument with default/bounded value passes signature check"))
-    my_client.on_new_point(callable=cl_on_new_point)
-
-    cl_connection_1.on_receive_raw(callable=cl_ct_on_receive_raw)
-    cl_connection_1.on_send_raw(callable=cl_ct_on_send_raw)
-
-    ##################################
-    # connect loop
-    ##################################
+    ### Register callbacks to print connection info
+    #register_callbacks(my_client, cl_connection_1)
 
     my_client.start()
 
     counter = 0
     while not cl_connection_1.is_connected and counter < 5:
-        print("CL] Waiting for connection to {0}:{1}".format(cl_connection_1.ip, cl_connection_1.port))
+        #print("CL] Waiting for connection to {0}:{1}".format(cl_connection_1.ip, cl_connection_1.port))
         time.sleep(1)
         counter += 1
 
-    ##################################
-    # Loop through points
-    ##################################
+    conpot = is_conpot(my_client)
 
-    cl_dump()
-    ##################################
-    # done
-    ##################################
+    ### Dump all point data stored in the ICS system that hosts the server
+    #cl_dump(my_client, cl_connection_1)
 
     my_client.stop()
 
     return conpot
-
---
+++ src/conpot_iec104_helper.py
@@ -0,0 +1,91 @@
+import functools
+import c104
+
+##################################
+# CONNECTION STATE HANDLER
+##################################
+
+def cl_ct_on_state_change(connection: c104.Connection, state: c104.ConnectionState) -> None:
+    print("CL] Connection State Changed {0} | State {1}".format(connection.originator_address, state))
+
+
+
+##################################
+# NEW DATA HANDLER
+##################################
+
+def cl_pt_on_receive_point(point: c104.Point, previous_info: c104.Information, message: c104.IncomingMessage) -> c104.ResponseState:
+    print("CL] {0} REPORT on IOA: {1}, message: {2}, previous: {3}, current: {4}".format(point.type, point.io_address, message, previous_info, point.info))
+    # print("{0}".format(message.is_negative))
+    # print("-->| POINT: 0x{0} | EXPLAIN: {1}".format(message.raw.hex(), c104.explain_bytes(apdu=message.raw)))
+    return c104.ResponseState.SUCCESS
+
+
+
+##################################
+# NEW OBJECT HANDLER
+##################################
+
+def cl_on_new_station(client: c104.Client, connection: c104.Connection, common_address: int, custom_arg: str, y: str = "default value") -> None:
+    print("CL] NEW STATION {0} | CLIENT OA {1}".format(common_address, client.originator_address))
+    connection.add_station(common_address=common_address)
+
+
+def cl_on_new_point(client: c104.Client, station: c104.Station, io_address: int, point_type: c104.Type) -> None:
+    print("CL] NEW POINT: {1} with IOA {0} | CLIENT OA {2}".format(io_address, point_type, client.originator_address))
+    point = station.add_point(io_address=io_address, type=point_type)
+    point.on_receive(callable=cl_pt_on_receive_point)
+
+
+
+##################################
+# RAW MESSAGE HANDLER
+##################################
+
+def cl_ct_on_receive_raw(connection: c104.Connection, data: bytes) -> None:
+    print("CL] <-in-- {1} [{0}] | CONN OA {2}".format(data.hex(), c104.explain_bytes_dict(apdu=data), connection.originator_address))
+
+
+def cl_ct_on_send_raw(connection: c104.Connection, data: bytes) -> None:
+    print("CL] -out-> {1} [{0}] | CONN OA {2}".format(data.hex(), c104.explain_bytes_dict(apdu=data), connection.originator_address))
+
+
+
+##################################
+# Dump points
+##################################
+
+def cl_dump(my_client, cl_connection_1):
+    
+    if cl_connection_1.is_connected:
+        print("")
+        cl_ct_count = len(my_client.connections)
+        print("CL] |--+ CLIENT has {0} connections".format(cl_ct_count))
+        for ct_iter in range(cl_ct_count):
+            ct = my_client.connections[ct_iter]
+            ct_st_count = len(ct.stations)
+            print("       |--+ CONNECTION has {0} stations".format(ct_st_count))
+            for st_iter in range(ct_st_count):
+                st = ct.stations[st_iter]
+                st_pt_count = len(st.points)
+
+                print("          |--+ STATION {0} has {1} points".format(st.common_address, st_pt_count))
+                print("             |      TYPE      |   IOA   |       VALUE        |        PROCESSED AT        |        RECORDED  AT        |      QUALITY      ")
+                print("             |----------------|---------|--------------------|----------------------------|----------------------------|-------------------")
+                for pt_iter in range(st_pt_count):
+                    pt = st.points[pt_iter]
+                    print("             | %s | %7s | %18s | %26s | %26s | %s" % (pt.type, pt.io_address, pt.value, pt.processed_at.isoformat(),
+                                                                                 pt.recorded_at and pt.recorded_at.isoformat() or 'N. A.', pt.quality))
+                    print("             |----------------|---------|--------------------|----------------------------|----------------------------|-------------------")
+
+
+def register_callbacks(my_client, cl_connection_1):
+
+    cl_connection_1.on_state_change(callable=cl_ct_on_state_change)
+
+    my_client.on_new_station(callable=functools.partial(cl_on_new_station,
+                                                        custom_arg="extra argument with default/bounded value passes signature check"))
+    my_client.on_new_point(callable=cl_on_new_point)
+
+    cl_connection_1.on_receive_raw(callable=cl_ct_on_receive_raw)
+    cl_connection_1.on_send_raw(callable=cl_ct_on_send_raw)
